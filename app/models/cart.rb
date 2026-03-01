2b1349872c63570165eadfce08a7e350d3f596c7
jacksonPrimo <jackson.primodev@gmail.com>
2026-02-28 23:58:06 -0300

    add cart item model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,20 @@
 class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  has_many :cart_items
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+
+  def add_item(product, quantity)
+    ActiveRecord::Base.transaction do
+      item = self.cart_items.find_or_initialize_by(product_id: product.id)
+      item.quantity = quantity
+      item.save!
+      update_total!      
+    end
+  end
+
+  def update_total!
+    total = cart_items.joins(:product).sum('cart_items.quantity * products.price')    
+    update!(total_price: total)
+  end
 end

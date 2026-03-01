37b412127a5e9b99d1c9123fe06fda0574b0872a
jacksonPrimo <jackson.primodev@gmail.com>
2026-03-01 00:06:17 -0300

    add middleware to handler errors in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :handle_internal_error
+  rescue_from ::CustomException, with: :handle_custom_exception
+
+  def handle_internal_error(exception)
+    render json: {
+      error: 'unexpected error',
+      details: exception.message.split(' for #<').first,
+      endpoint: "#{request.method} #{request.path}"
+    }, status: :internal_server_error
+  end
+
+  def handle_custom_exception(exception)
+    render json: { error: exception.message, details: exception.details }, status: exception.code
+  end
 end

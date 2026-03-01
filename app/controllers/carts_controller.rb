e1a71b30d403bfe22c7c19c6c58c98f39d786fbe
jacksonPrimo <jackson.primodev@gmail.com>
2026-03-01 10:39:21 -0300

    add endpoint to remove product from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,4 +8,13 @@ class CartsController < ApplicationController
     
     render json: cart, status: :ok
   end
+
+  def remove_item
+    cart = ::CartServices::RemoveItemService.new(
+      cart_id: params[:cart_id],
+      product_id: params[:product_id]
+    ).call
+
+    render json: cart, status: :ok
+  end
 end

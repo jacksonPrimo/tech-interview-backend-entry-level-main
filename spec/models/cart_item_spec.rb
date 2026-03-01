e1694365aae99d58b0d395a7c419f511f1d139f5
jacksonPrimo <jackson.primodev@gmail.com>
2026-03-01 18:55:17 -0300

    fix carts_routing and cart_item test cases

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it 'belongs to a cart' do
+      association = described_class.reflect_on_association(:cart)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a product' do
+      association = described_class.reflect_on_association(:product)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      cart = create(:cart)
+      product = create(:product)
+      cart_item = CartItem.new(cart: cart, product: product, quantity: 2)
+      expect(cart_item).to be_valid
+    end
+  end
 end

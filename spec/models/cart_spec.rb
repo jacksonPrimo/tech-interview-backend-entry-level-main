94c98421111478c8d4c202e248b8fcf759ed5f35
jacksonPrimo <jackson.primodev@gmail.com>
2026-03-01 10:43:16 -0300

    add test cases for 'remove product form cart' action

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -35,6 +35,32 @@ RSpec.describe Cart, type: :model do
     end
   end
 
+  describe 'remove_item' do
+    let(:shopping_cart) { create(:cart) }
+    let(:product) { create(:product, price: 50.0) }
+
+    before do
+      shopping_cart.add_item(product, 2)
+    end
+
+    it 'removes the item from the cart' do
+      shopping_cart.remove_item(product)
+      expect(shopping_cart.reload.cart_items.count).to eq(0)
+    end
+
+    it 'updates total_price correctly after removal' do
+      expect(shopping_cart.reload.total_price).to eq(100.0)
+      shopping_cart.remove_item(product)
+      expect(shopping_cart.reload.total_price).to eq(0.0)
+    end
+
+    it 'updates last_interaction_at' do
+      last_interaction_at_old = shopping_cart.reload.last_interaction_at
+      shopping_cart.remove_item(product)
+      expect(shopping_cart.reload.last_interaction_at).to be > last_interaction_at_old
+    end
+  end
+
   # describe 'remove_if_abandoned' do
   #   let(:shopping_cart) { create(:cart, last_interaction_at: 7.days.ago) }
 
